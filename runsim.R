dee999dfddd90ca179fa5caa55156ba793ec1d12
sophi890 <48591088+sophi890@users.noreply.github.com>
2023-06-19 19:52:33 -0400

    external file runs everything
    wls -> bin for spectralmethod

--- runsim.R
+++ runsim.R
@@ -12,14 +12,15 @@ source('funcs.R')
 
 # I first compute nest and spec in this chunk so they don't have to be recomputed
 outsim = sim(
-  n = 5,
+  n = 3,
+  l = 3,
   outcome = 'linear',
-  rhox = c(0.9, 0.001),
+  rhox = c(0.9, 0.5, 0.001),
   decomposition = 'nested',
   quiet = T
 )
 analnested = analysis(
-  n = 5,
+  n = 3,
   A = outsim$A,
   X = outsim$X,
   Y = outsim$Y,
@@ -29,7 +30,7 @@ analnested = analysis(
   return_decomps = T
 )
 analspectral = analysis(
-  n = 5,
+  n = 3,
   A = outsim$A,
   X = outsim$X,
   Y = outsim$Y,
@@ -43,122 +44,174 @@ spec = analspectral$spec
 
 out = list()
 # Sim 1
-out[[1]] = simfunc(
-  rhox = c(0.9, 0.001),
+out[[1]] = simfunc2(
+  n = 3,
+  l = 3,
+  rhox = c(0.9, 0.5, 0.001),
   dgm = 'nested',
   nest = nest,
   spec = spec,
-  spectralmethod = 'wls'
+  spectralmethod = 'bin'
 )
 
 # Sim 2
-out[[2]] = simfunc(
-  rhox = c(0.9, 0.001),
+out[[2]] = simfunc2(
+  n = 3,
+  l = 3,
+  rhox = c(0.9, 0.5, 0.001),
   dgm = 'nested',
   nest = nest,
   spec = spec,
   truncate = 1,
-  spectralmethod = 'wls'
+  spectralmethod = 'bin'
 )
 
 # Sim 3
-out[[3]] = simfunc(
-  rhox = seq(0, 1, by = 1 / (5 ^ 4 - 1)),
+out[[3]] = simfunc2(
+  n = 3,
+  l = 3,
+  rhox = seq(0, 1, by = 1 / (3 ^ 6 - 1)),
   dgm = 'spectral',
   nest = nest,
   spec = spec,
-  spectralmethod = 'wls'
+  spectralmethod = 'bin'
 )
 
 # Sim 4
-out[[4]] = simfunc(
-  rhox = c(rep(0, 525), seq(1 / 100, 1, by = 1 / 100)),
+out[[4]] = simfunc2(
+  n = 3,
+  l = 3,
+  rhox = c(rep(0, 3^6 - 100), seq(1 / 100, 1, by = 1 / 100)),
   dgm = 'spectral',
   nest = nest,
   spec = spec,
-  spectralmethod = 'wls'
+  spectralmethod = 'bin'
 )
 
 # Sim 5
-out[[5]] = simfunc(
-  rhox = c(0.9, 0.001),
+out[[5]] = simfunc2(
+  n = 3,
+  l = 3,
+  rhox = c(0.9, 0.5, 0.001),
   dgm = 'nested',
   nest = nest,
   spec = spec,
   objective = 'coherence',
-  spectralmethod = 'wls'
+  spectralmethod = 'bin'
 )
 
 # Sim 6
-out[[6]] = simfunc(
-  rhox = c(0.9, 0.001),
+out[[6]] = simfunc2(
+  n = 3,
+  l = 3,
+  rhox = c(0.9, 0.5, 0.001),
   dgm = 'nested',
   nest = nest,
   spec = spec,
   objective = 'coherence',
   truncate = 1,
-  spectralmethod = 'wls'
+  spectralmethod = 'bin'
 )
 
 # Sim 7
-out[[7]] = simfunc(
-  rhox = seq(0, 1, by = 1 / (5 ^ 4 - 1)),
+out[[7]] = simfunc2(
+  n = 3,
+  l = 3,
+  rhox = seq(0, 1, by = 1 / (3 ^ 6 - 1)),
   dgm = 'spectral',
   nest = nest,
   spec = spec,
   objective = 'coherence',
-  spectralmethod = 'wls'
+  spectralmethod = 'bin'
 )
 
 # Sim 8
-out[[8]] = simfunc(
-  rhox = seq(0, 1, by = 1 / (5 ^ 4 - 1)),
+out[[8]] = simfunc2(
+  n = 3,
+  l = 3,
+  rhox = seq(0, 1, by = 1 / (3 ^ 6 - 1)),
   dgm = 'spectral',
   nest = nest,
   spec = spec,
   objective = 'coherence',
   truncate = 312,
-  spectralmethod = 'wls'
+  spectralmethod = 'bin'
 )
 
 # Sim 9
-out[[9]] = simfunc(
-  rhox = c(0.001, 0.9),
+out[[9]] = simfunc2(
+  n = 3,
+  l = 3,
+  rhox = c(0.001, 0.5, 0.9),
   dgm = 'nested',
   nest = nest,
   spec = spec,
-  spectralmethod = 'wls'
+  spectralmethod = 'bin'
 )
 
 # Sim 10
-out[[10]] = simfunc(
-  rhox = seq(1, 0, by = -1 / (5 ^ 4 - 1)),
+out[[10]] = simfunc2(
+  n = 3,
+  l = 3,
+  rhox = seq(1, 0, by = -1 / (3 ^ 6 - 1)),
   dgm = 'spectral',
   nest = nest,
   spec = spec,
-  spectralmethod = 'wls'
+  spectralmethod = 'bin'
 )
 
 # Sim 11
-out[[11]] = simfunc(
-  rhox = c(0.9, 0.001),
+out[[11]] = simfunc2(
+  n = 3,
+  l = 3,
+  rhox = c(0.9, 0.5, 0.001),
   dgm = 'nested',
   nest = nest,
   spec = spec,
   outcome = 'interaction',
   betaxz = -1,
-  spectralmethod = 'wls'
+  spectralmethod = 'bin'
 )
 
 # Sim 12
-out[[12]] = simfunc(
-  rhox = seq(0, 1, by = 1 / (5 ^ 4 - 1)),
+out[[12]] = simfunc2(
+  n = 3,
+  l = 3,
+  rhox = seq(0, 1, by = 1 / (3 ^ 6 - 1)),
   dgm = 'spectral',
   nest = nest,
   spec = spec,
   outcome = 'interaction',
   betaxz = -1,
-  spectralmethod = 'wls'
+  spectralmethod = 'bin'
+)
+
+# Sim 13
+out[[13]] = simfunc2(
+  n = 3,
+  l = 3,
+  outcome = 'quadratic',
+  betax = c(2, 1),
+  rhox = c(0.9, 0.5, 0.001),
+  dgm = 'nested',
+  nest = nest,
+  spec = spec,
+  quiet = T,
+  spectralmethod = 'bin'
+)
+
+# Sim 14
+out[[14]] = simfunc2(
+  n = 3,
+  l = 3,  
+  outcome = 'quadratic', 
+  betax = c(2,1),
+  rhox = seq(0,1, by = 1/(3^6-1)),
+  dgm = 'spectral', 
+  nest = nest,
+  spec = spec,
+  quiet = T,
+  spectralmethod = 'bin'
 )
 
 save(out, file = 'out.Rdata')
