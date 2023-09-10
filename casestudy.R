4d4b4d2507be8e3b254d6c64556a6566c0e73e04
sophi890 <48591088+sophi890@users.noreply.github.com>
2023-09-10 19:51:39 -0400

    Update casestudy.R

--- casestudy.R
+++ casestudy.R
@@ -5,7 +5,7 @@ library(dplyr)
 library(ggpubr)
 library(gridExtra)
 
-scale = function(x, nbs){ # make sure order of $X$ matches A
+spscale = function(x, nbs){ # make sure order of $X$ matches A
   avgdists = rep(NA, length(x))
   for (node in 1:length(x)){
     avgdists[node] = mean(abs(x[node] - x[nbs[[node]]]), na.rm = T)
@@ -13,16 +13,9 @@ scale = function(x, nbs){ # make sure order of $X$ matches A
   return(avgdists)
 }
 
-#data(boston)
-#str(boston.c)
-#boston_sf <- boston.c %>% st_as_sf(., coords = c("LAT","LON"))
-#plot(boston_sf["CRIM"])
-#plot(boston_sf["MEDV"], logz = T)
-
 boston.tr <- sf::st_read(system.file("shapes/boston_tracts.shp",
                                      package="spData")[1])
-#boston.tr = boston.tr %>% mutate(across(where(is.numeric), scale))
-#bostonmerged <- merge(boston.tr, boston.c,by = "TOWN")
+
 g1 = ggplot(boston.tr) +
   geom_sf(aes(fill = DIS), col = NA, size = 0.005) + 
   theme_minimal() + 
@@ -58,7 +51,7 @@ dev.off()
 nri = read.csv('National_Risk_Index_Census_Tracts.csv')
 #nrow(nri) # 85154 census tracts
 cal = nri[nri$STATE == 'California',] # 9106
-caltr = st_read('/Users/sophie/Downloads/tl_2021_06_tract/tl_2021_06_tract.shp')
+caltr = st_read('tl_2021_06_tract/tl_2021_06_tract.shp')
 #nrow(caltr) # 9129
 #mean(cal$TRACTFIPS %in% as.numeric(caltr$GEOID)) # all covered
 caltr$TRACTFIPS = as.numeric(caltr$GEOID)
@@ -94,11 +87,11 @@ png('images/casestudyb.jpeg', height = 500, width = 1000)
 grid.arrange(g1, g2, ncol = 2)
 dev.off()
 
-#### Calculate scale of all variables ####
-nbs = poly2nb(boston.tr)
-mean(scale(boston.tr$DIS, nbs = nbs))
-mean(scale(boston.tr$logCRIM, nbs = nbs))
+#### Calculate spatial scale of all variables ####
+nbsbos = poly2nb(boston.tr)
+mean(spscale(scale(boston.tr$DIS), nbs = nbsbos))
+mean(spscale(scale(boston.tr$logCRIM), nbs = nbsbos))
 
-nbs = poly2nb(cal)
-mean(scale(calmerged$SOVI_SCORE, nbs = nbs))
-mean(scale(calmerged$HWAV_AFREQ, nbs = nbs))
+nbscal = poly2nb(calmerged)
+mean(spscale(scale(calmerged$SOVI_SCORE), nbs = nbscal), na.rm = T) # 2 NAs
+mean(spscale(scale(calmerged$HWAV_AFREQ), nbs = nbscal), na.rm = T)
