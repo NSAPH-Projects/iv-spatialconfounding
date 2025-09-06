419285c628465722fed99eca3d647e06b05ff49e
sophi890 <48591088+sophi890@users.noreply.github.com>
2025-09-06 18:38:58 -0400

    Update run_simfunc.R

--- simulation/run_simfunc.R
+++ simulation/run_simfunc.R
@@ -12,19 +12,22 @@ load('sim.RData')
 set.seed(123)
 args <- commandArgs(trailingOnly = TRUE)
 nsims <- as.integer(args[1])
-rangeu <- args[2]
+#rangeu <- args[2]
+confounding_mechanism <- args[2]
 option <- args[3]
 
-print(c(nsims, rangeu, option))
+#print(c(nsims, rangeu, option))
+print(c(nsims, confounding_mechanism, option))
 
 simfunc(nsims,
         simlist$lat,
         simlist$lon,
-        rangeu = rangeu,
+        #rangeu = rangeu,
+        confounding_mechanism = confounding_mechanism,
         option = option,
         GFT_conf = simlist$GFT_conf,
-        statemat = simlist$statemat,
-        within_state_GP = F # change to T for confounding scenario 3
+        statemat = simlist$statemat#,
+        #within_state_GP = F # change to T for confounding scenario 3
         )
 
 
