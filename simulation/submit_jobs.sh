52286f0556a238057f8ab25964b754a8ac06bcbc
sophi890 <48591088+sophi890@users.noreply.github.com>
2025-09-06 20:30:28 -0400

    update to accommodate new sim with 5 conf mech

--- simulation/submit_jobs.sh
+++ simulation/submit_jobs.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Define arrays for options and methods
-rangeus=("smallscale" "tinyscale") # Adjust as needed
+confounding_mechanisms=(1 2 3 4 5)
 options=("linear" "nonlinear")
 
 # Loop through each combination and submit jobs
-for rangeu in "${rangeus[@]}"; do
+for cm in "${confounding_mechanisms[@]}"; do
   for option in "${options[@]}"; do
-  sbatch --job-name=${rangeu}_${option}\
-    --output=output/${rangeu}_${option}.out \
-    --error=error/${rangeu}_${option}.err \
-    run_job.sh 500 "$rangeu" "$option"  
+  sbatch --job-name="cm${cm}_${option}" \
+      --output="output/cm${cm}_${option}.out" \
+      --error="error/cm${cm}_${option}.err" \
+      run_job.sh 500 "$cm" "$option" 
   sleep 1 # pause to be kind to the scheduler
   done
 done
