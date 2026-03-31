018de1f745ddaac7eb5ad03c9cb56dc66f65d19a
sophi890 <48591088+sophi890@users.noreply.github.com>
2026-03-31 17:32:18 -0400

    redo time and error files

--- simulation/run_job.sh
+++ simulation/run_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH -c 8 # Request 8 cores for parallel processing
-#SBATCH -t 00-03:00:00 # Amount of time needed DD-HH:MM:SS
+#SBATCH -t 00-24:00:00 # Amount of time needed DD-HH:MM:SS
 #SBATCH -p shared # Partition to submit to
 #SBATCH --mem=6000 # Memory
-#SBATCH -o /n/home07/swoodward/instrumental_variables_simulation/error.out #specify where to save errors returned by the program
-#SBATCH -e /n/home07/swoodward/instrumental_variables_simulation/log.err #specify where to save the output log
+#SBATCH -o error.out #specify where to save errors returned by the program
+#SBATCH -e log.err #specify where to save the output log
 #SBATCH --mail-type=END #notifications for job done
 #SBATCH --mail-user=swoodward@g.harvard.edu # send to address
 #SBATCH -N 1 #Number of nodes
