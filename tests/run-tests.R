5e55e7ecba6987ebc9cc423e260e59fd26c55b3d
sophi890 <48591088+sophi890@users.noreply.github.com>
2026-09-01 10:54:42 -0400

    removing data and csvs

--- tests/run-tests.R
+++
@@ -1,5 +0,0 @@
-testthat::test_dir(
-  file.path("tests", "testthat"),
-  reporter = "summary",
-  stop_on_failure = TRUE
-)
--- tests/testthat/helper-source.R
+++
@@ -1,4 +0,0 @@
-source(
-  testthat::test_path("..", "..", "R", "manuscript_basis_selection.R"),
-  local = FALSE
-)
